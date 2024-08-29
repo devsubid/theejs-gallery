90e968ed166ba9be6bf374d200c5d7e6ba33b9af
SUBID DAS <itsmesubid@gmail.com>
2024-08-29 22:53:32 +0530

    chore: Update dependencies and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Cylinder from "./cylinder";
-
+import {
+  Bloom,
+  EffectComposer,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing";
+import { Reflector } from "@react-three/drei";
 const App = () => {
   return (
     <div>
       <div className="h-screen w-screen">
         <Canvas
           camera={{
-            fov: 25,
-            zoom: 1,
+            fov: 28,
+            zoom: 0.9,
           }}
         >
           <OrbitControls
@@ -20,15 +26,36 @@ const App = () => {
             enableZoom={false}
           />
           <ambientLight intensity={2} />
-          {/* <pointLight position={[10, 10, 10]} /> */}
           <Cylinder />
+          <Reflector
+            blur={[1024, 1024]}
+            mixBlur={75}
+            mixStrength={0.25}
+            resolution={1024}
+            args={[50, 50]}
+            rotation={[-Math.PI * 0.5, 0, 0]}
+            mirror={2}
+            minDepthThreshold={0}
+            maxDepthThreshold={100}
+            depthScale={1000}
+            position={[0, -1.25, 0]}
+          >
+            {(Material: any, props) => (
+              <Material metalness={0.01} roughness={1} {...props} />
+            )}
+          </Reflector>
+          <EffectComposer>
+            <Bloom
+              mipmapBlur={true}
+              luminanceThreshold={0.01}
+              luminanceSmoothing={3}
+              height={300}
+              opacity={3}
+            />
+            <Noise opacity={0.025} />
+            <Vignette eskil={false} offset={0.1} darkness={0.5} />
+          </EffectComposer>
         </Canvas>
-      </div>
-      <div className="text-white">
-        <h1>React Three Fiber</h1>
-        <p>
-          This is a simple example of a cylinder with a texture applied to it.
-        </p>
       </div>
     </div>
   );
--- src/cylinder.tsx
+++ src/cylinder.tsx
@@ -1,27 +1,28 @@
 import * as THREE from "three";
-import texture from "./assets/texture.png";
-import { useTexture } from "@react-three/drei";
-// import { useFrame } from "@react-three/fiber";
+import texture from "/texture.png";
+import { MeshDistortMaterial, useTexture } from "@react-three/drei";
 import { Ref, useRef } from "react";
 const Cylinder = () => {
   const tex = useTexture(texture);
   const cylinderRef =
     useRef<THREE.Mesh<THREE.BufferGeometry, THREE.Material>>();
-  // useFrame((_, delta) => {
-  //   if (cylinderRef.current) {
-  //     cylinderRef.current.rotation.y += 0.25 * delta;
-  //   }
-  // });
   return (
-    <group rotation={[-0.15, 0.35, 0.1]}>
+    <group
+    // rotation={[-0.15, 0.35, 0.1]}
+    >
       <mesh
         ref={
           cylinderRef as Ref<THREE.Mesh<THREE.BufferGeometry, THREE.Material>>
         }
         scale={[-1, 1, 1]}
       >
-        <cylinderGeometry args={[3, 3, 2.1, 60, 1, true, Math.PI / 2]} />
-        <meshStandardMaterial map={tex} transparent side={THREE.BackSide} />
+        <cylinderGeometry args={[3, 3, 2.1, 1000, 100, true, Math.PI / 2]} />
+        <MeshDistortMaterial
+          map={tex}
+          transparent
+          side={THREE.BackSide}
+          distort={0.4}
+        />
       </mesh>
     </group>
   );
